f193c7d7f183efaac7bc43bef28017c7ea465e59
sachinaryan912 <sachinsachu1998.ss@gmail.com>
2025-05-24 20:09:15 +0530

    updated fix

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-# Stage 1: Build with Maven
+
 FROM maven:3.9.4-eclipse-temurin-17 AS builder
 WORKDIR /app
 COPY . .
 RUN mvn clean package -DskipTests
 
-# Stage 2: Run with JDK
+
 FROM openjdk:17-jdk-slim
 WORKDIR /app
 COPY --from=builder /app/target/algospectra-backend-0.0.1-SNAPSHOT.jar app.jar
--- src/main/java/org/company/algospectra_backend/config/CorsConfig.java
+++ src/main/java/org/company/algospectra_backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")  // or "*" if needed
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization")
