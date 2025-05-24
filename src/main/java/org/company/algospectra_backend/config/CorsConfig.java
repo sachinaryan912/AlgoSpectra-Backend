7a4851cf63487adb6aa97edbdf111be67a554b49
sachinaryan912 <sachinsachu1998.ss@gmail.com>
2025-05-24 17:13:52 +0530

    fixed cors

---
+++ src/main/java/org/company/algospectra_backend/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package org.company.algospectra_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")  // or "*" if needed
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
